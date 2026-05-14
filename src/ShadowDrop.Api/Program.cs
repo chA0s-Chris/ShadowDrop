0661cd0e1a07826afe87af78de27a50b9d26bad7
Christian Flessa <chris@z0rn.net>
2026-05-14 18:49:20 +0200

    chore: add projects

---
+++ src/ShadowDrop.Api/Program.cs
@@ -0,0 +1,3 @@
+﻿// Copyright (c) 2026 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+Console.WriteLine("API");
